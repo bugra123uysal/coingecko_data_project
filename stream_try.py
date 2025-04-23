e75f80d5dbbb1493c440bb78a7917232b2fda241
buğra uysal <uysalbugra134@gmail.com>
2025-04-23 03:26:10 +0300

    I added a dataset containing new coins and coin prices, I will start querying them in Google Colab.

--- stream_try.py
+++ stream_try.py
@@ -3,23 +3,38 @@ import requests
 import time
 from datetime import datetime
 
-stock = ["bitcoin", "ethereum", "solana", "tether", "binancecoin"]
+stock = [ "bitcoin", "ethereum", "tether", "binancecoin", "solana",
+    "ripple", "usd-coin", "cardano", "avalanche-2", "dogecoin",
+    "toncoin", "polkadot", "chainlink", "polygon", "wrapped-bitcoin",
+    "shiba-inu", "tron", "uniswap", "litecoin", "internet-computer",
+    "dai", "bitcoin-cash", "ethereum-classic", "cosmos", "stellar",
+    "okb", "monero", "lido-dao", "aptos", "filecoin",
+    "hedera", "arbitrum", "crypto-com-chain", "vechain", "near",
+    "immutable-x", "optimism", "quant-network", "the-graph", "algorand",
+    "render-token", "aave", "flow", "elrond-erd-2", "stacks",
+    "the-sandbox", "axie-infinity", "tezos", "theta-token", "decentraland"]
 while True:
-     prices=[]
-     for symbol in stock:
-         url=f"https://api.coingecko.com/api/v3/simple/price?ids={symbol}&vs_currencies=usd"
-         aa =requests.get(url).json()
-         price=aa[symbol]['usd']
-         print(f"[{datetime.now().strftime('%Y-%m-%d  %H:%M:%S')}] {symbol.capitalize()}: ${price:.2f}")
-         prices.append([datetime.now(), symbol, price ])
-    
-     df=pd.DataFrame(prices, columns=["Data", "Price", "Coin"])
-     df.to_csv("crypto_prc.csv", mode='w' ,header=True , index=False  )
+     try:
+      
+      prices=[]
+      idy=",".join(stock)
+      url=f"https://api.coingecko.com/api/v3/simple/price?ids={idy}&vs_currencies=usd"
+      data=requests.get(url).json()
+      for symbol in stock:     
+            if symbol in data:                                                                                                                                                                                 
+                 price=data[symbol]['usd']
+                 print(f"[{datetime.now().strftime('%Y-%m-%d  %H:%M:%S')}] {symbol.capitalize()}: ${price:.2f}")
+                 prices.append([datetime.now(), symbol, price ])    
+     
+            else:
+                print("sorun oluştu sorun için şimdiden özür dileriz")       
+      df=pd.DataFrame(prices, columns=["Data", "Price", "Coin"])
+      df.to_csv("crypto_prc.csv", mode='w' ,header=True , index=False  )
 
 
-
-     time.sleep(60)
-
+      time.sleep(60)
 
 
-
+     except Exception as e:                                                                                                                                                                  
+      print("tekrar deneyiniz : " , e)
+      time.sleep(60)
