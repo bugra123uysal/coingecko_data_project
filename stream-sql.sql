fff64204c934561af53641e3c66af08aae4941f0
buğra uysal <uysalbugra134@gmail.com>
2025-05-14 01:13:21 +0300

    Added SQL queries to sort by year, month and day

--- stream-sql.sql
+++ stream-sql.sql
@@ -21,7 +21,14 @@ SELECT "hours_24_change"  FROM crypto_prices WHERE hours_24_change > 0 ORDER BY 
 
 SELECT "hours_24_change" FROM crypto_prices WHERE hours_24_change < 0 ORDER BY hours_24_change ASC
 SELECT "hours_24_change" FROM crypto_prices WHERE hours_24_change < 0 ORDER BY hours_24_change DESC
- 
+
+select "Coin", "Price","year" from crypto_prices order by year asc
+select "Coin","Price","year" from crypto_prices  order by year desc
+select "Coin","Price","month" from crypto_prices  order by month asc
+select "Coin","Price","month" from crypto_prices  order by month desc
+select "Coin","Price","day" from crypto_prices  order by day  asc
+select "Coin","Price","day" from crypto_prices order by day desc
+ asc
 SELECT "Date",
 EXTRACT(YEAR FROM "Date")AS year,
 EXTRACT(MONTH FROM "Date" )AS month,
